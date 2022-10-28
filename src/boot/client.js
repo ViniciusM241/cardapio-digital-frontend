0f151a2a7d53352efa0240f28dbd47689667ba42
Vinicius Melo <viniciusmelo241@gmail.com>
2022-10-27 22:09:20 -0300

    client url

--- src/boot/client.js
+++ src/boot/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken, removeToken } from './auth';
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL + '/api',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 client.interceptors.request.use((config) => {
